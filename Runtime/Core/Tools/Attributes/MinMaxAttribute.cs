7457ca3f3d2b4b4084c25d2c485923b33acecf5d
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-02-07 21:40:06 +0100

    Replace MinMax draw Label -> FloatField

--- Runtime/Core/Tools/Attributes/MinMaxAttribute.cs
+++ Runtime/Core/Tools/Attributes/MinMaxAttribute.cs
@@ -4,19 +4,19 @@ namespace BaseTool.Tools.Attributes
 {
     public class MinMaxAttribute : PropertyAttribute
     {
-        public float minLimit = 0;
-        public float maxLimit = 1;
+        public float MinLimit { get; } = 0;
+        public float MaxLimit { get; } = 1;
 
         public MinMaxAttribute(int min, int max)
         {
-            minLimit = min;
-            maxLimit = max;
+            MinLimit = min;
+            MaxLimit = max;
         }
 
         public MinMaxAttribute(float min, float max)
         {
-            minLimit = min;
-            maxLimit = max;
+            MinLimit = min;
+            MaxLimit = max;
         }
     }
 }
