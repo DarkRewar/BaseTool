6bc03af33016e40b26c43ef63edd674a714470a4
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-03-04 18:15:11 +0100

    Add ReadOnly Attribute

---
+++ Editor/Core/Tools/Drawers/ReadOnlyDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BaseTool.Tools.Drawers
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    internal class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = true;
+        }
+    }
+}
