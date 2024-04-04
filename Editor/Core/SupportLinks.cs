6ccac0152d04db21a82764e03aa29d6b5816186d
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-04-04 17:57:44 +0200

    Add #32 - Support Links

---
+++ Editor/Core/SupportLinks.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BaseTool.Editor
+{
+    public static class SupportLinks
+    {
+        [MenuItem("Window/BaseTool/Documentation", false, 100)]
+        public static void OpenDocumentation()
+        {
+            Application.OpenURL("https://github.com/DarkRewar/BaseTool?tab=readme-ov-file#documentation");
+        }
+
+        [MenuItem("Window/BaseTool/Report a bug...", false, 101)]
+        public static void OpenIssues()
+        {
+            Application.OpenURL("https://github.com/DarkRewar/BaseTool/issues");
+        }
+    }
+}
