2d8d8dd9c958d218a2180ee25dc39f92088ec0f3
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-05-03 12:35:27 +0200

    Fix ReadOnly height bug

--- Editor/Core/Tools/Drawers/ReadOnlyDrawer.cs
+++ Editor/Core/Tools/Drawers/ReadOnlyDrawer.cs
@@ -6,11 +6,23 @@ namespace BaseTool.Tools.Drawers
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     internal class ReadOnlyDrawer : PropertyDrawer
     {
+        private bool _displayed;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            _displayed = position.height > 1f;
+            if (!_displayed) return;
+
             GUI.enabled = false;
             EditorGUI.PropertyField(position, property, label);
             GUI.enabled = true;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (_displayed)
+                return base.GetPropertyHeight(property, label);
+            return 0;
         }
     }
 }
