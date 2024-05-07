e0d570de9d0bfde2fc327a6dd5e289c18d1586f4
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-05-07 13:35:12 +0200

    TickManager #43

---
+++ Runtime/Core/Exceptions/NotASingletonException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BaseTool
+{
+    internal class NotASingletonException : Exception
+    {
+        public NotASingletonException() : base() { }
+
+        public NotASingletonException(string message) : base(message) { }
+    }
+}
