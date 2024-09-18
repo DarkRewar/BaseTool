3b80234ea0f840c2dc52766e637737ba1c2f4e9b
Curtis Pelissier <curtis.pelissier@gmail.com>
2024-09-18 22:55:11 +0200

    Add IsPointInsidePolygon method #108

--- Tests/Runtime/MathUtilsTest.cs
+++ Tests/Runtime/MathUtilsTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -59,6 +60,82 @@ namespace BaseTool.Tests
             Assert.IsFalse(MathUtils.Approximately(smallVector.magnitude, 0, 1E-24f));
         }
 
+        #endregion
+        
+        #region MathUtils.IsPointInsidePolygon
+
+        private List<Vector2> GenerateSquare() => new()
+        {
+            new(0, 0),
+            new(0, 1),
+            new(1, 1),
+            new(1, 0)
+        };
+        
+        private List<Vector2> GenerateOctagon() => new()
+        {
+            new(1, 0),
+            new(2, 0),
+            new(3, 1),
+            new(3, 2),
+            new(2, 3),
+            new(1, 3),
+            new(0, 2),
+            new(0, 1)
+        };
+        
+        private List<Vector2> GenerateV() => new()
+        {
+            new(0, 0),
+            new(3, 3),
+            new(2, 3),
+            new(0, 1),
+            new(-2, 3),
+            new(-3, 3)
+        };
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is inside a square")]
+        public void MathUtilsPointIsInsideSquare()
+        {
+            List<Vector2> points = GenerateSquare();
+            Assert.IsTrue(MathUtils.IsPointInsidePolygon(new Vector2(0.5f, 0.5f), points));
+        }
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is not inside a square")]
+        public void MathUtilsPointIsOutsideSquare()
+        {
+            List<Vector2> points = GenerateSquare();
+            Assert.IsFalse(MathUtils.IsPointInsidePolygon(new Vector2(-1, -1), points));
+        }
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is inside an octagon")]
+        public void MathUtilsPointIsInsideOctagon()
+        {
+            List<Vector2> points = GenerateOctagon();
+            Assert.IsTrue(MathUtils.IsPointInsidePolygon(new Vector2(2.5f, 1.5f), points));
+        }
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is outside an octagon")]
+        public void MathUtilsPointIsOutsideOctagon()
+        {
+            List<Vector2> points = GenerateOctagon();
+            Assert.IsFalse(MathUtils.IsPointInsidePolygon(new Vector2(3, 3), points));
+        }
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is inside a complex polygon")]
+        public void MathUtilsPointIsInsideComplexPolygon()
+        {
+            List<Vector2> points = GenerateV();
+            Assert.IsTrue(MathUtils.IsPointInsidePolygon(new Vector2(-1f, 1.5f), points));
+        }
+        
+        [Test(Author = "DarkRewar", Description = "Test if a point is outside a complex polygon")]
+        public void MathUtilsPointIsOutsideComplexPolygon()
+        {
+            List<Vector2> points = GenerateV();
+            Assert.IsFalse(MathUtils.IsPointInsidePolygon(new Vector2(0, 2), points));
+        }
+        
         #endregion
     }
 }
